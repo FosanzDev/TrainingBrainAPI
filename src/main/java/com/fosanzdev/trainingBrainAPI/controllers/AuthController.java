73208c25e71ecd0ea26a6e3e4b795d2f7ff616d5
fosanz <fosanzdev@gmail.com>
2024-05-11 14:36:57 +0200

    Adapted HTTP1 controller to work like gRPC controller

--- src/main/java/com/fosanzdev/trainingBrainAPI/controllers/AuthController.java
+++ src/main/java/com/fosanzdev/trainingBrainAPI/controllers/AuthController.java
@@ -1,5 +1,8 @@
 package com.fosanzdev.trainingBrainAPI.controllers;
 
+import com.fosanzdev.trainingBrainAPI.models.AccessToken;
+import com.fosanzdev.trainingBrainAPI.models.AuthCode;
+import com.fosanzdev.trainingBrainAPI.models.RefreshToken;
 import com.fosanzdev.trainingBrainAPI.services.auth.interfaces.IAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,25 +28,99 @@ public class AuthController {
         String username = body.get("username");
         String password = body.get("password");
 
-        Map<String, String> response = new HashMap<>();
-        response.put("access_token", "token" + username + password);
-        return ResponseEntity.ok(response);
+        boolean validAccount = authService.verifyAccount(username, password, false);
+
+        if (validAccount) {
+            authService.forceLogout(username);
+            AuthCode code = authService.createAuthCode(username);
+
+            Map<String, String> response = new HashMap<>();
+            response.put("authToken", code.getCode());
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/verify")
-    void verify(@RequestBody Map<String, String> body) {
+    ResponseEntity<Map<String, String>> verify(@RequestBody Map<String, String> body) {
+        //Get username, password and authToken from body
+        String username = body.get("username");
+        String password = body.get("password");
+        String authToken = body.get("authToken");
+
+        //Verify the auth code
+        boolean validAccount = authService.verifyAccount(username, password, true);
+        boolean validAuthCode = authService.validateAuthCode(authToken, username);
+        if (validAccount && validAuthCode) {
+            authService.invalidateAuthCode(authToken);
+            RefreshToken refreshToken = authService.createRefreshToken(username);
+            AccessToken accessToken = authService.createAccessToken(username);
+
+            Map<String, String> response = new HashMap<>();
+            response.put("refreshToken", refreshToken.getToken());
+            response.put("accessToken", accessToken.getToken());
+
+            return ResponseEntity.ok(response);
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 
     @PostMapping("/refresh")
-    void refresh(@RequestBody Map<String, String> body) {
+    ResponseEntity<Map<String, String>> refresh(@RequestBody Map<String, String> body) {
+        // Get the request parameters
+        String refreshToken = body.get("refreshToken");
+        String accessToken = body.get("accessToken");
+
+        // Validate the sent tokens
+        boolean valid = authService.validateRefreshToken(refreshToken, accessToken);
+
+        if (valid) {
+            // Refresh the access token and return it
+            AccessToken newAccessToken = authService.refreshAccessToken(refreshToken, accessToken);
+
+            Map<String, String> response = new HashMap<>();
+            response.put("accessToken", newAccessToken.getToken());
+
+            return ResponseEntity.ok(response);
+
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
     }
 
     @PostMapping("/register")
     void register(@RequestBody Map<String, String> body) {
+        //Get username, name and password from body
+        String username = body.get("username");
+        String name = body.get("name");
+        String password = body.get("password");
+
+        AuthCode authCode = authService.register(name, username, password);
+
+        if (authCode != null) {
+            Map<String, String> response = new HashMap<>();
+            response.put("authToken", authCode.getCode());
+            ResponseEntity.ok(response);
+        } else {
+            // Return 409 (conflict) if the account already exists
+            ResponseEntity.status(409).build();
+        }
     }
 
     @PostMapping("/logout")
     void logout(@RequestBody Map<String, String> body) {
+        // Get the request parameters
+        String username = body.get("username");
+        String refreshToken = body.get("refreshToken");
+
+        boolean success = authService.logout(username, refreshToken);
+        if (success) {
+            ResponseEntity.ok().build();
+        } else {
+            ResponseEntity.badRequest().build();
+        }
     }
 
 }
