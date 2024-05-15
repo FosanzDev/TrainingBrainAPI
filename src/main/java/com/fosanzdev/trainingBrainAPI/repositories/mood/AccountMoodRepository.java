717ccae7c7906674e188b473d963c06cee9bde02
fosanz <fosanzdev@gmail.com>
2024-05-15 18:20:37 +0200

    Added mood history order

--- src/main/java/com/fosanzdev/trainingBrainAPI/repositories/mood/AccountMoodRepository.java
+++ src/main/java/com/fosanzdev/trainingBrainAPI/repositories/mood/AccountMoodRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface AccountMoodRepository extends JpaRepository<AccountMood, Long> {
 
     //NOTE: Using native query to avoid the use of the entity manager in order to use limit and offset
-    @Query(value="SELECT * FROM account_moods WHERE fk_account = ?1 LIMIT ?2 OFFSET ?3", nativeQuery = true)
+    @Query(value="SELECT * FROM account_moods WHERE fk_account = ?1 ORDER BY date DESC LIMIT ?2 OFFSET ?3", nativeQuery = true)
     List<AccountMood> findByAccountWithLimit(String accountId, int limit, int offset);
 
     @Query(value="SELECT * FROM account_moods WHERE fk_account = ?1", nativeQuery = true)
