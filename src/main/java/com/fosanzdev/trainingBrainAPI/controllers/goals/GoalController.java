5e60c291bdd1830817899a4e8d661c331259312d
fosanz <fosanzdev@gmail.com>
2024-05-25 22:12:53 +0200

    Cleared some dependencies, changed version

--- src/main/java/com/fosanzdev/trainingBrainAPI/controllers/goals/GoalController.java
+++ src/main/java/com/fosanzdev/trainingBrainAPI/controllers/goals/GoalController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +19,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RequestMapping("/goals")
 @RestController
