78a81a2534add71920b29ce46a7eaacbd841bc4a
fosanz <fosanzdev@gmail.com>
2024-05-26 19:18:03 +0200

    Removed unnecessary classes

--- src/main/java/com/fosanzdev/trainingBrainAPI/repositories/goals/RoutineEntryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fosanzdev.trainingBrainAPI.repositories.goals;
-
-import com.fosanzdev.trainingBrainAPI.models.goals.RoutineEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoutineEntryRepository extends JpaRepository<RoutineEntry, String> {
-}
