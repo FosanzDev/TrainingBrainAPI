ae4a8f7c008c04ee788ad1f3d253a57a440fd5a2
fosanz <fosanzdev@gmail.com>
2024-05-26 23:22:39 +0200

    Added routine docs

--- src/main/java/com/fosanzdev/trainingBrainAPI/controllers/goals/RoutineController.java
+++ src/main/java/com/fosanzdev/trainingBrainAPI/controllers/goals/RoutineController.java
@@ -4,8 +4,12 @@ import com.fosanzdev.trainingBrainAPI.models.details.User;
 import com.fosanzdev.trainingBrainAPI.models.goals.Routine;
 import com.fosanzdev.trainingBrainAPI.services.interfaces.data.IUserDataService;
 import com.fosanzdev.trainingBrainAPI.services.interfaces.goals.IRoutineService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +20,7 @@ import java.util.Map;
 
 @RequestMapping("/routines")
 @RestController
-@Tag(name = "Routine", description = "Routine management")
+@Tag(name = "Routine", description = "Manejo de rutinas")
 public class RoutineController {
 
     @Autowired
@@ -25,6 +29,30 @@ public class RoutineController {
     @Autowired
     private IUserDataService userDataService;
 
+    @Operation(summary = "Añade una nueva rutina", description = "Añade una nueva rutina al usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Routine added",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Routine added"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "400", description = "Routine malformed",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Routine malformed"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Unauthorized"
+                                    }
+                                    """))),
+    })
     @PostMapping("/add")
     ResponseEntity<Map<String, Object>> addRoutine(
             @Parameter(description = "Token de autorización", required = true, example = "Bearer <token>")
@@ -62,8 +90,44 @@ public class RoutineController {
         }
     }
 
+    @Operation(summary = "Lista de rutinas para hoy", description = "Lista las rutinas en un marco de 24 horas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rutinas listadas",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "routines": [
+                                            {
+                                                "id": "1",
+                                                "title": "Rutina de ejercicios",
+                                                "description": "Rutina de ejercicios para mantenerse en forma",
+                                                "every": 1,
+                                                "routineType": "DAYS",
+                                                "startTime": "2021-09-01T00:00:00Z",
+                                                "endTime": "2021-09-30T00:00:00Z",
+                                                "submissionDate": "2021-09-01T00:00:00Z"
+                                            }
+                                        ]
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Unauthorized"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "500", description = "Error getting routines",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Error getting routines"
+                                    }
+                                    """))),
+    })
     @GetMapping("/today")
     ResponseEntity<Map<String, Object>> getRoutinesToday(
+            @Parameter(description = "Token de autorización", required = true, example="Bearer <token>")
             @RequestHeader("Authorization") String bearer
     ) {
         try {
@@ -81,6 +145,45 @@ public class RoutineController {
         }
     }
 
+
+    @Operation(summary = "Obtener una rutina", description = "Obtiene una rutina por su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rutina obtenida",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "id": "1",
+                                        "title": "Rutina de ejercicios",
+                                        "description": "Rutina de ejercicios para mantenerse en forma",
+                                        "every": 1,
+                                        "routineType": "DAYS",
+                                        "startTime": "2021-09-01T00:00:00Z",
+                                        "endTime": "2021-09-30T00:00:00Z",
+                                        "submissionDate": "2021-09-01T00:00:00Z"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Unauthorized"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "404", description = "Rutina no encontrada",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Routine not found"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "500", description = "Error getting routine",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Error getting routine"
+                                    }
+                                    """))),
+    })
     @GetMapping("/get/{id}")
     ResponseEntity<Map<String, Object>> getRoutine(
             @RequestHeader("Authorization") String bearer,
@@ -101,6 +204,41 @@ public class RoutineController {
         }
     }
 
+    @Operation(summary = "Obtener todas las rutinas", description = "Obtiene todas las rutinas del usuario")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rutinas obtenidas",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "routines": [
+                                            {
+                                                "id": "1",
+                                                "title": "Rutina de ejercicios",
+                                                "description": "Rutina de ejercicios para mantenerse en forma",
+                                                "every": 1,
+                                                "routineType": "DAYS",
+                                                "startTime": "2021-09-01T00:00:00Z",
+                                                "endTime": "2021-09-30T00:00:00Z",
+                                                "submissionDate": "2021-09-01T00:00:00Z"
+                                            }
+                                        ]
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Unauthorized"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "500", description = "Error getting routines",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Error getting routines"
+                                    }
+                                    """))),
+    })
     @GetMapping("/all")
     ResponseEntity<Map<String, Object>> getAllRoutines(
             @RequestHeader("Authorization") String bearer
@@ -120,6 +258,37 @@ public class RoutineController {
         }
     }
 
+    @Operation(summary = "Eliminar una rutina", description = "Elimina una rutina por su ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rutina eliminada",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Routine deleted"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Unauthorized"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "404", description = "Rutina no encontrada",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Routine not found"
+                                    }
+                                    """))),
+            @ApiResponse(responseCode = "500", description = "Error deleting routine",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(example = """
+                                    {
+                                        "message": "Error deleting routine"
+                                    }
+                                    """))),
+    })
     @DeleteMapping("/delete/{id}")
     ResponseEntity<Map<String, Object>> deleteRoutine(
             @RequestHeader("Authorization") String bearer,
